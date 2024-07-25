e2d9b60fb752037656f224c3de91575a46f5c8e4
Ciaran Sweet <9111975+ciaransweet@users.noreply.github.com>
2024-07-25 15:44:29 +0100

    Allow for the source dataset to be specified in the CLI (#5)
    * Allow for the source dataset to be specified in the CLI
    
    * Use actual default value not empty string
    
    * Fix issue with using some typing not supported in 3.9

---
+++ src/hazard/sources/__init__.py
@@ -0,0 +1,22 @@
+from typing import Literal, Mapping, Dict, Any, Protocol, Type, Callable
+
+from hazard.protocols import OpenDataset
+from hazard.sources.nex_gddp_cmip6 import NexGddpCmip6
+from hazard.sources.ukcp18_rcp85 import Ukcp18Rcp85
+from hazard.sources.wri_aqueduct import WRIAqueductSource
+
+SourceDataset = Literal[
+    "NEX-GDDP-CMIP6",
+    "UKCP18",
+]
+
+_SOURCE_DATASETS: Mapping[str, Callable[..., OpenDataset]] = {
+    "NEX-GDDP-CMIP6": NexGddpCmip6,
+    "UKCP18": Ukcp18Rcp85,
+}
+
+
+def get_source_dataset_instance(source_dataset: SourceDataset, source_dataset_kwargs: Dict[str, Any]) -> OpenDataset:
+    if source_dataset not in _SOURCE_DATASETS:
+        raise ValueError(f"Invalid source dataset: {source_dataset}")
+    return _SOURCE_DATASETS[source_dataset](**source_dataset_kwargs)
